57b206898d22b6fedb52ffd3a0ebf4e002b9cdee
Mateusz Barwicki <72599764+Persiwal@users.noreply.github.com>
2024-07-13 14:57:36 +0200

    style: Layout

--- frontend/src/components/base/layout/Layout.tsx
+++ frontend/src/components/base/layout/Layout.tsx
@@ -1,26 +1,44 @@
 import Header from './header/Header'
-import { Outlet } from 'react-router-dom'
-import { CssBaseline } from '@mui/material'
+import { Outlet, useLocation } from 'react-router-dom'
+import { Box, CssBaseline } from '@mui/material'
 import Container from '@mui/material/Container'
-import Sidebar from './sidebar/Sidebar'
 import Breadcrumb from './breadcrumb/Breadcrumb'
 import MainContentContainer from '../../ui/main-content-container/MainContentContainer'
+import BackToMainPageButton from '../../ui/back-to-main-page-button/BackToMainPageButton'
+
+const layoutContainerStyles = {
+  display: 'flex',
+  flexDirection: 'column',
+  minHeight: '100vh',
+}
+
+const mainContainerStyles = { mt: 4, mb: 4 }
+
+const currentPageNavStyles = {
+  height: '36px',
+  display: 'flex',
+  gap: 4,
+  alignItems: 'center',
+}
 
 const Layout = () => {
+  const location = useLocation()
+  const currentPath = location.pathname
+
   return (
-    <>
+    <Box sx={layoutContainerStyles}>
       <CssBaseline />
       <Header />
-      <Container maxWidth="xl" sx={{ display: 'flex', mt: 4, mb: 4 }}>
-        <Sidebar />
-        <Container sx={{ p: 0 }}>
+      <Container maxWidth="xl" sx={mainContainerStyles}>
+        <Box sx={currentPageNavStyles}>
+          {currentPath.startsWith('/modules/') && <BackToMainPageButton />}
           <Breadcrumb />
-          <MainContentContainer>
-            <Outlet />
-          </MainContentContainer>
-        </Container>
+        </Box>
+        <MainContentContainer>
+          <Outlet />
+        </MainContentContainer>
       </Container>
-    </>
+    </Box>
   )
 }
 
