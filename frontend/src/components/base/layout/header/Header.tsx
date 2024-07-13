945135177c0ef2e101e3b3adbc1e5668112749c5
Mateusz Barwicki <72599764+Persiwal@users.noreply.github.com>
2024-07-13 14:57:10 +0200

    style: add height for Header

--- frontend/src/components/base/layout/header/Header.tsx
+++ frontend/src/components/base/layout/header/Header.tsx
@@ -3,9 +3,11 @@ import Toolbar from '@mui/material/Toolbar'
 import Container from '@mui/material/Container'
 import Logo from './Logo'
 
+const containerStyles = { height: '64px' }
+
 const Header = () => {
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={containerStyles}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Logo />
