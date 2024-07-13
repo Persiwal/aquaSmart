2b92d24c3bcd6fd8735d11238ab72c0019e1605c
Mateusz Barwicki <72599764+Persiwal@users.noreply.github.com>
2024-07-13 13:55:16 +0200

    refactor: Breadcrumb

--- frontend/src/components/base/layout/breadcrumb/Breadcrumb.tsx
+++ frontend/src/components/base/layout/breadcrumb/Breadcrumb.tsx
@@ -2,34 +2,26 @@ import { Link, useLocation } from 'react-router-dom'
 import { Box, Typography, Breadcrumbs } from '@mui/material'
 import formatPath from '../../../../utils/formatPath/formatPath'
 
+const containerStyles = { width: '100%', marginBottom: '10px', height: '24px' }
+
 const Breadcrumb = () => {
   const location = useLocation()
   const currentPath = location.pathname
   const breadcrumbs = formatPath(currentPath)
 
   if (currentPath === '/modules') {
-    return <div style={{ height: '24px' }}></div>
+    return <Box sx={containerStyles}></Box>
   }
 
   return (
-    <Box sx={{ width: '100%' }}>
-      <Box sx={{ marginBottom: '10px' }}>
-        <Breadcrumbs aria-label="breadcrumb">
-          {breadcrumbs.map((crumb, index) => (
-            <Link
-              key={index}
-              to={crumb.url}
-              style={{
-                textDecoration: 'none',
-                color:
-                  index === breadcrumbs.length - 1 ? 'text.primary' : 'inherit',
-              }}
-            >
-              <Typography color="textPrimary">{crumb.label}</Typography>
-            </Link>
-          ))}
-        </Breadcrumbs>
-      </Box>
+    <Box sx={containerStyles}>
+      <Breadcrumbs aria-label="breadcrumb">
+        {breadcrumbs.map((crumb, index) => (
+          <Link key={index} to={crumb.url}>
+            <Typography>{crumb.label}</Typography>
+          </Link>
+        ))}
+      </Breadcrumbs>
     </Box>
   )
 }
