772f7e197c0685c53791450964374b2e3d6329c6
Mateusz Barwicki <72599764+Persiwal@users.noreply.github.com>
2024-07-13 14:59:44 +0200

    style: MainContentContainer component

--- frontend/src/components/ui/main-content-container/MainContentContainer.tsx
+++ frontend/src/components/ui/main-content-container/MainContentContainer.tsx
@@ -5,8 +5,17 @@ interface Props {
   children: React.ReactNode
 }
 
+const containerStyles = {
+  flexGrow: 1,
+  minHeight: 'calc(100vh - 64px - 64px - 36px)', // assuming header height is 64px and breadcrumb height is 24
+}
+
 const MainContentContainer: React.FC<Props> = ({ children }) => {
-  return <Box component="main">{children}</Box>
+  return (
+    <Box component="main" sx={containerStyles}>
+      {children}
+    </Box>
+  )
 }
 
 export default MainContentContainer
