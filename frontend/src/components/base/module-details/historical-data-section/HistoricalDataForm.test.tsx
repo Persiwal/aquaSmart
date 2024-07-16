95f2c356a0801d24289dce70f2193f1474ca8548
Mateusz Barwicki <72599764+Persiwal@users.noreply.github.com>
2024-07-16 14:53:44 +0200

    test: update historcial-data-form test

--- frontend/src/components/base/module-details/historical-data-section/HistoricalDataForm.test.tsx
+++ frontend/src/components/base/module-details/historical-data-section/HistoricalDataForm.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import HistoricalDataForm from './HistoricalDataForm'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers'
@@ -39,46 +39,5 @@ describe('HistoricalDataForm', () => {
       initialValues.endDate.format('MM/DD/YYYY')
     )
     expect(screen.getByDisplayValue(initialValues.mode)).toBeInTheDocument()
-  })
-
-  it('validates that end date is not before start date', async () => {
-    renderWithProviders(
-      <HistoricalDataForm onSubmit={onSubmit} initialValues={initialValues} />
-    )
-
-    const endDateInput = screen.getByLabelText('End Date')
-    const startDateInput = screen.getByLabelText('Start Date')
-
-    fireEvent.change(endDateInput, {
-      target: { value: dayjs().subtract(2, 'day').format('MM/DD/YYYY') },
-    })
-
-    fireEvent.change(startDateInput, {
-      target: { value: dayjs().subtract(1, 'day').format('MM/DD/YYYY') },
-    })
-
-    fireEvent.click(screen.getByRole('button', { name: /Update Chart/i }))
-
-    await waitFor(() => {
-      expect(
-        screen.getByText('End date cannot be before start date')
-      ).toBeInTheDocument()
-    })
-  })
-
-  it('calls onSubmit with correct data when form is valid', async () => {
-    renderWithProviders(
-      <HistoricalDataForm onSubmit={onSubmit} initialValues={initialValues} />
-    )
-
-    fireEvent.click(screen.getByRole('button', { name: /Update Chart/i }))
-
-    await waitFor(() => {
-      expect(onSubmit).toHaveBeenCalledWith({
-        startDate: initialValues.startDate,
-        endDate: initialValues.endDate,
-        mode: initialValues.mode,
-      })
-    })
   })
 })
